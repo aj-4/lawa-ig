223996ac0818645883647b91eeb896532f6b7947
Aaron Pietsch <apietsch4@gmail.com>
2018-01-13 22:30:30 -0800

    remove console logs

--- client/src/components/main_feed_pg/feed.jsx
+++ client/src/components/main_feed_pg/feed.jsx
@@ -101,7 +101,6 @@ class OneFeed extends React.Component {
   //toggles the 'heart' img of a like/unlike post. also sends user liked to database
 	toggleLike() {
     //PUSH picture data into logged in user's liked photos
-
     var bodyObj = {userId: this.props.loggedInUser.user_id, postId: this.props.post};
 
     bodyObj.status = this.state.liked ? 'rmLike' : 'addLike';
@@ -146,7 +145,6 @@ class OneFeed extends React.Component {
       this.setState({newComment: ''});
       this.nameInput.value = '';      
     }
-    console.log('loading.');
     this.loadComments(this.props.post)
   }
 
@@ -214,4 +212,3 @@ class OneFeed extends React.Component {
 }
 
 export default OneFeed;
-
